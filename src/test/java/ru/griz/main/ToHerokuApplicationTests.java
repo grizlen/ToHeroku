50a0fbdc32cc40cf45e4a97ec5b4da0c54069585
griz <grizlen@rambler.ru>
2021-12-13 15:04:30 +0500

    Initial commit

---
+++ src/test/java/ru/griz/main/ToHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.griz.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
